ba9c726f6167f12b350e97b8a33921a66647cbd8
Николай Сафонов <uporoty-cot@mail.ru>
2025-09-07 03:03:50 +0300

    add prettifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,132 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from 'react';
+import Image from 'next/image'
+import Editor from '@monaco-editor/react';
+
+function prettifyJson(code: string): string {
+  return JSON.stringify(JSON.parse(code), null, '\t')
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/tools/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [code, setCode] = useState('');
+  const [editorLoaded, setEditorLoaded] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/tools/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/tools/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/tools/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/tools/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const editor = <Editor
+    value={code}
+    onChange={(value) => setCode(value)}
+    defaultLanguage='json'
+    options={{
+      lineNumbers: 'off',
+      folding: false,
+      minimap: {
+        enabled: false,
+      },
+      quickSuggestions: false,
+      renderLineHighlight: 'none',
+      matchBrackets: 'never',
+      codeLens: false,
+      autoIndent: 'none',
+      cursorBlinking: 'solid',
+      guides: {
+        bracketPairs: false,
+        bracketPairsHorizontal: false,
+        highlightActiveBracketPair: false,
+        highlightActiveIndentation: false,
+        indentation: false,
+      },
+      hover: {
+        enabled: false,
+      },
+      stickyScroll: {
+        enabled: false,
+      },
+      // scrollbar: {
+      // vertical: 'hidden',
+      // horizontal: 'hidden'
+      // },
+      glyphMargin: false,
+      lineDecorationsWidth: 0,
+      lineNumbersMinChars: 0,
+      scrollBeyondLastLine: false,
+      occurrencesHighlight: 'off',
+      hideCursorInOverviewRuler: true,
+      overviewRulerBorder: false,
+      fontSize: 16,
+    }}
+    onMount={(_, monaco) => {
+      monaco.editor.defineTheme('custom', {
+        base: 'vs-dark',
+        rules: [
+          { token: '', foreground: '#ffffffff' },
+          // { token: 'number', foreground: '#09885A' },
+
+          { token: 'string.key.json', foreground: '#14d9e3ff' },
+          { token: 'string.value.json', foreground: '#0c59b2ff' },
+
+          // { token: 'string', foreground: '#0c59b2ff' },
+
+
+          // { token: 'string', foreground: '#A31515' },
+          { token: 'keyword.json', foreground: '#8a63ffff' },
+        ],
+        colors: {
+          'editor.background': '#11141e',
+          'editor.foreground': '#ffffffff',
+        },
+        inherit: true,
+      })
+      monaco.editor.setTheme('custom')
+      setEditorLoaded(true)
+    }}
+  />
+
+  function handleBadJson() {
+    // todo highlight editor
+    console.log("Bad json")
+  }
+
+  return (
+    <div style={{
+      'display': 'flex',
+      'flexDirection': 'column',
+      'alignItems': 'center',
+      'gap': '30px',
+    }}>
+      <Image src={'/tools/logo.png'} alt={'Logo'} width={200} height={200}></Image>
+
+      <div style={{
+        'display': 'flex',
+        'flexDirection': 'row',
+        'gap': '10px',
+      }}>
+        <button className='button' onClick={() => {
+          try {
+            setCode(prettifyJson(code))
+          } catch {
+            handleBadJson()
+          }
+        }
+        }>
+          Prettify
+        </button>
+        <button className='button' onClick={() => {
+          navigator.clipboard.writeText(code)
+        }}>
+          Copy
+        </button>
+      </div>
+
+      <div hidden={!editorLoaded} style={{
+        'borderRadius': '20px',
+        'overflow': 'hidden',
+        'width': '800px',
+        'height': '600px',
+        'padding': '10px',
+        'backgroundColor': '#11141e',
+      }}>
+        {editor}
+      </div>
     </div>
   );
 }
